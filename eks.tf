a2ca2ffae064688fddf4a5b872f037351073a0f5
isouza99 <isaias.souza@dentsu.com>
2023-07-04 22:51:46 -0300

    chore: Test Fig

--- eks.tf
+++ eks.tf
@@ -1,83 +1,101 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.2"
-
-  cluster_name    = local.name
-  cluster_version = "1.27"
-
-  cluster_endpoint_public_access = true
-
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = [
-    module.vpc.public_subnets[0],
-    module.vpc.public_subnets[1],
-    module.vpc.public_subnets[1]
-  ]
-
-    # EKS Managed Node Group(s)
-    eks_managed_node_group_defaults = {
-      instance_types = ["t3.micro", "t3.small", "t3.medium", "t3.large"]
-    }
-
-    eks_managed_node_groups = {
-      blue = {}
-      green = {
-        min_size     = 2
-        max_size     = 5
-        desired_size = 2
-
-        instance_types = ["t3.small"]
-        capacity_type  = "ON_DEMAND"
-      }
-    }
-
-#  manage_aws_auth_configmap = true
+#module "eks" {
+#  source  = "terraform-aws-modules/eks/aws"
+#  version = "19.15.2"
 #
-#  aws_auth_roles = [
-#    {
-#      rolearn  = "$ARN"
-#      username = "$USER"
-#      groups   = ["system:masters"]
-#    },
+#  cluster_name    = local.name
+#  cluster_version = "1.27"
+#
+#  cluster_endpoint_public_access = true
+#
+#  cluster_addons = {
+#    coredns = {
+#      most_recent = true
+#    }
+#    kube-proxy = {
+#      most_recent = true
+#    }
+#    vpc-cni = {
+#      most_recent = true
+#    }
+#  }
+#
+#  vpc_id = module.vpc.vpc_id
+#  subnet_ids = [
+#    module.vpc.private_subnets[0],
+#    module.vpc.private_subnets[1],
+#    module.vpc.private_subnets[1]
 #  ]
-
-
-  tags = merge(
-    var.tags,
-    {
-      Name = local.name
-    },
-  )
-
-  depends_on = [
-    module.vpc
-  ]
-}
-
-#data "aws_eks_cluster" "default" {
-#  name = module.eks.cluster_name
-#}
+#  #
+#  #    # EKS Managed Node Group(s)
+#  #    eks_managed_node_group_defaults = {
+#  #      instance_types = ["t3.micro", "t3.small", "t3.medium", "t3.large"]
+#  #    }
+#  #
+#  #    eks_managed_node_groups = {
+#  #      blue = {}
+#  #      green = {
+#  #        min_size     = 2
+#  #        max_size     = 5
+#  #        desired_size = 2
+#  #
+#  #        instance_types = ["t3.small"]
+#  #        capacity_type  = "ON_DEMAND"
+#  #      }
+#  #    }
+##
+##  # Fargate Profile(s)
+##  fargate_profiles = {
+##    default = {
+##      name = "default"
+##      selectors = [
+##        {
+##          namespace = "default"
+##        },
+##        {
+##          namespace = "cert-manager"
+##        },
+##        {
+##          namespace = "opentelemetry-operator-system"
+##        }
+##      ]
+##    }
+##  }
 #
-#data "aws_eks_cluster_auth" "default" {
-#  name = module.eks.cluster_name
-#}
+#  #  manage_aws_auth_configmap = true
+#  #
+#  #  aws_auth_roles = [
+#  #    {
+#  #      rolearn  = "$ARN"
+#  #      username = "$USER"
+#  #      groups   = ["system:masters"]
+#  #    },
+#  #  ]
 #
-#provider "kubernetes" {
-#  host                   = data.aws_eks_cluster.default.endpoint
-#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-#  token                  = data.aws_eks_cluster_auth.default.token
+#
+#  tags = merge(
+#    var.tags,
+#    {
+#      Name = local.name
+#    },
+#  )
+#
 #  depends_on = [
-#    module.eks]
+#    module.vpc
+#  ]
 #}
+#
+##data "aws_eks_cluster" "default" {
+##  name = module.eks.cluster_name
+##}
+##
+##data "aws_eks_cluster_auth" "default" {
+##  name = module.eks.cluster_name
+##}
+##
+##provider "kubernetes" {
+##  host                   = data.aws_eks_cluster.default.endpoint
+##  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+##  token                  = data.aws_eks_cluster_auth.default.token
+##  depends_on = [
+##    module.eks]
+##}
--- vpc.tf
+++ vpc.tf
@@ -6,8 +6,12 @@ module "vpc" {
   name = local.name
   cidr = local.vpc_cird
 
-  azs = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
-  public_subnets = ["10.79.0.0/24", "10.79.1.0/24", "10.79.2.0/24"]
+  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  private_subnets = ["10.79.0.0/24", "10.79.1.0/24", "10.79.2.0/24"]
+  public_subnets  = ["10.79.3.0/24", "10.79.4.0/24", "10.79.5.0/24"]
+  #database_subnets = ["10.79.6.0/24", "10.79.7.0/24", "10.79.8.0/24"]
+
+  #create_database_subnet_group = true
 
   manage_default_network_acl = true
   default_network_acl_tags   = { Name = "${local.name}-default" }
@@ -21,7 +25,13 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  map_public_ip_on_launch = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+  flow_log_max_aggregation_interval    = 60
 
   tags = merge(
     var.tags,
